3e4317ff2e2395e8bdbcfd2d011acfc901b15fb3
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-08-24 23:00:27 +0300

    customise admin panel

--- tendering/admin.py
+++ tendering/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from tendering.models import Category, Comment, User
+
+admin.site.unregister(Group)
+admin.site.register(Comment)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", ]
+    list_filter = ["name", ]
+    search_fields = ["name", ]
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display
+    list_filter = UserAdmin.list_filter
+    fieldsets = UserAdmin.fieldsets
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("first_name", "last_name",)}),
+    )
