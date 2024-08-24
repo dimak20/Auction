a9719079517cb67aea6c9d8b0e585246b28e183a
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-08-24 23:16:52 +0300

    add DB to gitignore and create index in views.py

--- tendering/views.py
+++ tendering/views.py
@@ -1,3 +1,22 @@
+from http.client import HTTPResponse
+from lib2to3.fixes.fix_input import context
+
+from django.http import HttpRequest
 from django.shortcuts import render
 
+from tendering.models import Category, User, Lot, Comment
+
+
 # Create your views here.
+def index(request: HttpRequest) -> HTTPResponse:
+    num_categories = Category.objects.count()
+    num_users = User.objects.count()
+    num_lots = Lot.objects.count()
+    active_lots = Lot.objects.filter(is_active=True).count()
+    context = {
+        "num_categories": num_categories,
+        "num_users": num_users,
+        "num_lots": num_lots,
+        "active_lots": active_lots
+    }
+    return render(request, "tendering/index.html", context=context)
