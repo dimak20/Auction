b7a676ec8a8b5190b384b5305c74b6aa2ff553d2
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-08-24 21:44:43 +0300

    modify setting with user model and add Lot model

--- tendering/models.py
+++ tendering/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -21,6 +22,22 @@ class User(AbstractUser):
 
 
 class Comment(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="comments")
     lot = models.ForeignKey("Lot", on_delete=models.CASCADE, related_name="comments")
     text = models.TextField(max_length=1000)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Lot(models.Model):
+    name = models.CharField(max_length=63)
+    description = models.TextField(max_length=1000)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, related_name="lots")
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    start_price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    current_price = models.DecimalField(max_digits=10, decimal_places=2)
+    participant = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="active_lots")
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="lots")
+
+
