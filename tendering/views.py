e489773584cbb6926f202e1b327d9a2b485ed19d
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-08-29 22:41:09 +0300

    Customise active users table in index

--- tendering/views.py
+++ tendering/views.py
@@ -1,5 +1,5 @@
 from http.client import HTTPResponse
-from django.db.models import Sum
+from django.db.models import Sum, Avg, Count
 from django.contrib.auth import login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db import transaction
@@ -27,13 +27,17 @@ def index(request: HttpRequest) -> HTTPResponse:
     num_active_lots = Lot.objects.filter(is_active=True).count()
     num_bids = Bid.objects.count()
     sum_lots = Lot.objects.aggregate(total=Sum("current_price"))
+    lot_participants = Lot.objects.annotate(p_c=Count("participant"))
+    avg_bids = lot_participants.aggregate(avg=Avg("p_c"))
+
     context = {
         "num_categories": num_categories,
         "num_users": num_users,
         "num_lots": num_lots,
         "num_active_lots": num_active_lots,
         "num_bids": num_bids,
-        "sum_lots": sum_lots["total"]
+        "sum_lots": sum_lots["total"],
+        "avg_bids": round(avg_bids["avg"], 2)
     }
     return render(request, "pages/index.html", context=context)
 
