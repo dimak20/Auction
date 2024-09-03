55c5b90f5c2567fae92b5292e3387af2bfc04422
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-09-04 02:17:20 +0300

    Update AWS settings

--- Auction/settings.py
+++ Auction/settings.py
@@ -166,6 +166,5 @@ else:
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
-    MEDIA_ROOT = "media/"
 
 ADMIN_URL = "admin/"
