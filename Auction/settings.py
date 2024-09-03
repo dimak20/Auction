f94bc75e91c08dc230ede8afa0050f543a5be098
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-09-04 00:39:33 +0300

    Change s3 bucket

--- Auction/settings.py
+++ Auction/settings.py
@@ -161,7 +161,7 @@ if DEBUG:
 else:
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     AWS_S3_REGION_NAME = "eu-north-1"
-    AWS_STORAGE_BUCKET_NAME = "auctionbucketdimak"
+    AWS_STORAGE_BUCKET_NAME = "newauctionbucket"
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
