37394d26e3dcb2d7049306639bd7912d3d259c85
Dmytro Kolchak <dima.kolhac@gmail.com>
2024-09-04 00:11:35 +0300

    Update custom aws media domain

--- Auction/settings.py
+++ Auction/settings.py
@@ -162,7 +162,7 @@ else:
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     AWS_S3_REGION_NAME = "eu-north-1"
     AWS_STORAGE_BUCKET_NAME = "auctionbucketdimak"
-    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com"
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
